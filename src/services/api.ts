21c9b555c50106488cad46f4da8057bf73388619
devfel <devfelizardo@gmail.com>
2025-02-10 18:17:39 -0300

    changing the url for the react_app

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL || 'http://54.164.96.252:3333',
 })
 
 // const api = axios.create({
